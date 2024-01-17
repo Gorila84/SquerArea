c01a9f963c8a946a378156c823ce7f14ceb28932
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-17 23:28:28 +0100

    Add replace decimal on point

--- SquerArea/Program.cs
+++ SquerArea/Program.cs
@@ -10,10 +10,12 @@ NumberStyles style = NumberStyles.AllowDecimalPoint;
 
 Console.WriteLine("Wpisz wysokośc prostokąta (format 0,00):");
 squereHeight = Console.ReadLine();
+squereHeight = squereHeight.Replace(',', '.');
 double.TryParse(squereHeight, style, CultureInfo.InvariantCulture, out height);
 
 Console.WriteLine("Wpisz szerokość prostokąta (format 0,00):");
 squereWidth = Console.ReadLine();
+squereWidth = squereWidth.Replace(',', '.');
 double.TryParse(squereWidth, style, CultureInfo.InvariantCulture, out width);
 
 diagonal = Math.Sqrt((Math.Pow(width, 2) + Math.Pow(height, 2)));
